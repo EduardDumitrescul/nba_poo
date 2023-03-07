5b95061cf121bb4a1539887c43cf0f8bd4f30342
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-03-07 12:35:38 +0200

    added classes with attributes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,48 @@
 #include <iostream>
+#include <vector>
+
+class Player {
+    static int idCount;
+    const int playerId;
+
+    char* name;
+    int birthYear;
+    float height;
+    char* position;
+    bool injured;
+};
+
+class Team {
+    static int idCount;
+    const int teamId;
+
+    char* name;
+    int* lastFiveGamesRecord;
+    std::vector <Player> playerList;
+};
+
+class Game {
+    static int idCount;
+    const int gameId;
+
+    Team homeTeam;
+    Team awayTeam;
+    Player mvp;
+
+    int homeTeamScore;
+    int awayTeamScore;
+};
+
+class Season {
+    static int idCount;
+    const int seasonId;
+
+    int startYear;
+    std::vector <Game> games;
+};
+
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
